e049a69cde6aefcabd88f1902382c18b1202d656
Prithvi Prabahar <prithvi923@gmail.com>
2012-10-17 18:01:49 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Episodik::Application.config.secret_token = '6a54188856c041684f50c3da286ffca622ec3de33ec590c1c005d8a91d22fc58f1ee48adbae942e92ba6600952d4229e27d6abcbc1079ba00ca08e1b71b1a039'
