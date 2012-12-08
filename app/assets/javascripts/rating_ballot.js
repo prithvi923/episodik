a6abd11b6d083ec3f31ec3d50468ac1b99b3a59a
Prithvi Prabahar <prithvi923@gmail.com>
2012-12-08 14:28:24 -0500

    Fix rating_ballot.js

--- app/assets/javascripts/rating_ballot.js
+++ app/assets/javascripts/rating_ballot.js
@@ -23,6 +23,5 @@ $(document).ready(function() {
     // Submits the form (saves data) after user makes a change.
     $('form.rating_ballot').change(function() {
         $('form.rating_ballot').submit();
-        if (!checkedId) location.reload();
     });
 });
