b5b040648f627fbf66f3201c40bd9d591211d44f
Prithvi Prabahar <prithvi923@gmail.com>
2012-12-09 03:04:17 -0500

    Fix rating_ballot

--- app/helpers/tvshows_helper.rb
+++ app/helpers/tvshows_helper.rb
@@ -1,6 +1,6 @@
 module TvshowsHelper
   def rating_ballot
-    if @history = current_user.histories.find_by_show_id(params[:id])
+    if @history = current_user.histories.find_by_show_id(params[:show_id])
       @history
     else
       current_user.histories.new
@@ -8,7 +8,7 @@ module TvshowsHelper
   end
 
   def current_user_rating
-    if @history = current_user.histories.find_by_show_id(params[:id])
+    if @history = current_user.histories.find_by_show_id(params[:show_id])
         @history.rating
     else
         "N/A"
